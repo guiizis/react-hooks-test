55c574acae30c40fc13ab805aaa41f2fa80f8a60
guiziis <guigamoda@hotmail.com>
2023-06-30 10:04:16 -0300

    chore: testes com useReducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 /* eslint-disable prettier/prettier */
-import { Div } from './components/Div'
-import React from 'react';
+import { useReducer } from 'react';
 import './App.css';
-import { AppContext } from './contexts/app';
+
+export const globalState = {
+  title: 'o titulo do contexto',
+  body: 'o body do contexto',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+
+  switch (action.type) {
+    case 'change':
+      console.log(action.payload);
+      return { ...state, title: 'change ' + action.payload };
+    case 'invert':
+      console.log('inverter');
+      return { ...state, title: state.title.split('').reverse().join('') };
+  }
+
+  return { ...state };
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
+
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>{title}</h1>
+      <p>{counter}</p>
+      <p>{body}</p>
+      <button onClick={() => dispatch({ type: 'change', payload: new Date().toLocaleDateString('pt-br') })}>click</button>
+      <button onClick={() => dispatch({ type: 'invert' })}>invert</button>
+    </div>
   );
 }
 
