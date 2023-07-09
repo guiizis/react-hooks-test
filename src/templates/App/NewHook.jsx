7974108dc8160383f19a4145812fb750d6a9086e
guiziis <guigamoda@hotmail.com>
2023-07-09 17:18:53 -0300

    chore: testes com useDebug

--- src/templates/App/NewHook.jsx
+++ src/templates/App/NewHook.jsx
@@ -1,28 +1,44 @@
-import { useLayoutEffect, useRef, useState } from 'react';
+import {
+  useDebugValue,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
+
+const useMediaQuery = (queryValue) => {
+  const [match, setMatch] = useState(false);
+
+  useDebugValue('teste');
+
+  useEffect(() => {
+    console.log(queryValue);
+    let isMounted = true;
+    const matchMedia = window.matchMedia(queryValue);
+    const handleChange = () => {
+      if (!isMounted) return;
+      setMatch(!!matchMedia.match);
+    };
+
+    matchMedia.addEventListener('change', handleChange);
+    setMatch(!!matchMedia.matches);
+
+    return () => {
+      isMounted = false;
+      matchMedia.removeEventListener('change', handleChange);
+    };
+  }, [queryValue]);
+
+  return match;
+};
 
 export const Home = () => {
-  const [counted, setCounted] = useState([0, 1, 2, 3, 4]);
-  const divRef = useRef();
-
-  const handleClick = () => {
-    setCounted((c) => [...c, Number(counted.slice(-1)) + 1]);
-  };
-
-  useLayoutEffect(() => {
-    divRef.current.scrollTop = divRef.current.scrollHeight;
-  }, []);
+  const huge = useMediaQuery('(min-width: 980px)');
+  const background = huge ? 'green' : '';
 
   return (
-    <>
-      <button onClick={handleClick}>Count {counted.slice(-1)}</button>
-      <div
-        ref={divRef}
-        style={{ height: '200px', width: '100px', overflow: 'scroll' }}
-      >
-        {counted.map((e) => {
-          return <p key={e}>{e}</p>;
-        })}
-      </div>
-    </>
+    <div style={{ background }}>
+      <h1>teste</h1>
+    </div>
   );
 };
