5441ddd17d1085bd8c840b9e7b50753e368d8520
guiziis <guigamoda@hotmail.com>
2023-07-16 12:52:32 -0300

    chore: testando useHistory e useLocation

--- src/components/Abc/index.jsx
+++ src/components/Abc/index.jsx
@@ -1,7 +1,12 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation, useHistory } from 'react-router-dom';
 
 export const Abc = () => {
   const { id } = useParams();
+  const location = useLocation();
+  const history = useHistory();
+
+  console.log(location);
+  console.log(history);
 
   return <h1>{id}</h1>;
 };
